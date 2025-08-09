4d596a674ce50c7e7216eefdfb05859bfacdb783
theaihopgg <theaihopgg@gmail.com>
2025-08-09 19:12:03 +0700

    fix: guilds settings service

--- bot/services/guilds_settings/service.py
+++ bot/services/guilds_settings/service.py
@@ -1,3 +1,25 @@
-"""The service provides base interaction with guilds settings"""
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from .crud import get_guild_settings, initialize_guild_settings
+from ...core.models import GuildSettings
+
+
+async def get_guild_settings(session: AsyncSession, *, guild_id: int) -> GuildSettings | None:
+    return await (
+        await session.execute(
+            select(GuildSettings).where(GuildSettings.guild_id == guild_id),
+        )
+    ).scalar_one_or_none()
+
+
+async def initialize_guild_settings(session: AsyncSession, *, guild_id: int) -> bool:
+    # check is guild settings exists
+    if not await get_guild_settings(session, guild_id=guild_id):
+        session.add(GuildSettings(guild_id=guild_id))
+        await session.commit()
+        return True
+    else:
+        return False
+
+
+__all__ = ("get_guild_settings", "initialize_guild_settings")
