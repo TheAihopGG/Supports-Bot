9453e18a07a699c15798115cffdf03f65a63a4b3
theaihopgg <theaihopgg@gmail.com>
2025-08-09 19:27:55 +0700

    feat: added set commands to common cog

--- bot/cogs/common/cog.py
+++ bot/cogs/common/cog.py
@@ -1,9 +1,9 @@
 from disnake.ext import commands
-from disnake import AppCmdInter
+from disnake import AppCmdInter, Role
 
 from ...core.database import session_factory
 from ...core.models import GuildSettings
-from ...services.guilds_settings import initialize_guild_settings
+from ...services.guilds_settings import initialize_guild_settings, get_guild_settings
 
 
 class CommonCog(commands.Cog):
@@ -12,6 +12,49 @@ class CommonCog(commands.Cog):
         if inter.author.guild_permissions.administrator:
             async with session_factory() as session:
                 if await initialize_guild_settings(session, guild_id=inter.guild_id):
+                    await inter.response.send_message(content="Успешно")
+                else:
+                    await inter.response.send_message(content="Сервер уже настроен")
+        else:
+            await inter.response.send_message(content="Недостаточно прав")
+
+    @commands.slash_command()
+    async def set(
+        self,
+        inter: AppCmdInter,
+    ) -> None:
+        pass
+
+    @set.sub_command()
+    async def set_female_role(
+        self,
+        inter: AppCmdInter,
+        role: Role,
+    ) -> None:
+        if inter.author.guild_permissions.administrator:
+            async with session_factory() as session:
+                if guild_settings := await get_guild_settings(session, guild_id=inter.guild_id):
+                    guild_settings.female_role_id = role.id
+                    await session.refresh(guild_settings)
+                    await session.commit()
+                    await inter.response.send_message(content="Успешно")
+                else:
+                    await inter.response.send_message(content="Сервер уже настроен")
+        else:
+            await inter.response.send_message(content="Недостаточно прав")
+
+    @set.sub_command()
+    async def set_male_role(
+        self,
+        inter: AppCmdInter,
+        role: Role,
+    ) -> None:
+        if inter.author.guild_permissions.administrator:
+            async with session_factory() as session:
+                if guild_settings := await get_guild_settings(session, guild_id=inter.guild_id):
+                    guild_settings.male_role_id = role.id
+                    await session.refresh(guild_settings)
+                    await session.commit()
                     await inter.response.send_message(content="Успешно")
                 else:
                     await inter.response.send_message(content="Сервер уже настроен")
