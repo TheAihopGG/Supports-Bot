878c894d877692b4d9b8f219aeb2626092b1e6e5
theaihopgg <theaihopgg@gmail.com>
2025-08-09 20:24:57 +0700

    fix

--- bot/cogs/verify/cog.py
+++ bot/cogs/verify/cog.py
@@ -2,8 +2,8 @@ from disnake.ext import commands
 from disnake import AppCmdInter, Member, Role
 
 from ...core.database import session_factory
-from ...core.models import GuildSettings
 from ...services.guilds_settings import get_guild_settings
+from ...services.user import get_or_create_user_by_discord_id
 
 
 class VerifyCog(commands.Cog):
@@ -19,13 +19,17 @@ class VerifyCog(commands.Cog):
                 if support_role := inter.guild.get_role(guild_settings.support_role_id):
                     # if author is in support staff
                     if support_role in inter.author.roles:
-                        # if gender_role is valid
-                        if gender_role.id in {guild_settings.male_role_id, guild_settings.female_role_id}:
-                            await member.add_roles(gender_role)
-                            await member.send(content="Вы успешно верифицированы")
-                            await inter.response.send_message(content="Вы успешно верифицировали участника")
+                        user = await get_or_create_user_by_discord_id(session, discord_id=member.id)
+                        if not user.is_verified:
+                            # if gender_role is valid
+                            if gender_role.id in {guild_settings.male_role_id, guild_settings.female_role_id}:
+                                await member.add_roles(gender_role)
+                                await member.send(content="Вы успешно верифицированы")
+                                await inter.response.send_message(content="Вы успешно верифицировали участника")
+                            else:
+                                await inter.response.send_message(content="Некорректная роль", ephemeral=True)
                         else:
-                            await inter.response.send_message(content="Некорректная роль", ephemeral=True)
+                            await inter.response.send_message(content="Участник уже верифицирован", ephemeral=True)
                     else:
                         await inter.response.send_message(content="Вы не состоите в составе поддержки сервера", ephemeral=True)
                 else:
