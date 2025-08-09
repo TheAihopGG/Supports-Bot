f04fba07b51ca4209d3c88abcf4990e516319300
theaihopgg <theaihopgg@gmail.com>
2025-08-09 19:24:30 +0700

    fix: models __init__ file

--- bot/core/models/__init__.py
+++ bot/core/models/__init__.py
@@ -3,5 +3,6 @@ The package contains files with class inherits from `sqlalchemy.Model` inside.
 """
 
 from .base import Base
+from .guild_settings import GuildSettings
 
-__all__ = ("Base",)
+__all__ = ("Base", "GuildSettings")
