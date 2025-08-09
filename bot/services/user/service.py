fd23cc8a6156fbf19f2c74b85c62dd94e3d3043d
theaihopgg <theaihopgg@gmail.com>
2025-08-09 20:25:12 +0700

    feat: user service

---
+++ bot/services/user/service.py
@@ -0,0 +1,21 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from ...core.models import User
+
+
+async def get_or_create_user_by_discord_id(session: AsyncSession, *, discord_id: int) -> User:
+    if user := (
+        await session.execute(
+            select(User).where(User.discord_id == discord_id),
+        )
+    ).scalar_one_or_none():
+        return user
+    else:
+        user = User(discord_id=discord_id)
+        session.add(user)
+        await session.commit()
+        return user
+
+
+__all__ = ("get_or_create_user_by_discord_id",)
