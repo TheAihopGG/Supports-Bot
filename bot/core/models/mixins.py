e63f1f56e0e3110ae9e0e107346352e4efa401f6
theaihopgg <theaihopgg@gmail.com>
2025-08-09 21:27:38 +0700

    fix: GuildIDMixin

--- bot/core/models/mixins.py
+++ bot/core/models/mixins.py
@@ -9,11 +9,10 @@ class IDMixin:
 
 
 class GuildIDMixin:
-    _is_guild_id_foreign_key = False
 
     @declared_attr
     def guild_id(cls) -> Mapped[int]:
-        return mapped_column(unique=True, nullable=False) if cls._is_guild_id_foreign_key else mapped_column(ForeignKey("guilds_settings.guild_id"), nullable=False)
+        return mapped_column(unique=True, nullable=False)
 
 
 __all__ = ("IDMixin",)
