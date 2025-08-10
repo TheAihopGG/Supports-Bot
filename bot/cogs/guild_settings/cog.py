9959b128ab12f312c5ede0878b8ab55877548f0c
theaihopgg <theaihopgg@gmail.com>
2025-08-10 17:57:58 +0700

    feat: guild_settings commands docs

--- bot/cogs/guild_settings/cog.py
+++ bot/cogs/guild_settings/cog.py
@@ -1,20 +1,21 @@
 from disnake.ext import commands
+from disnake.ext.commands import Param
 from disnake import AppCmdInter, Member, Role
 
 from ...core.database import session_factory
 from ...core.embeds import NotEnoughPermissionsEmbed, GuildWasNotSetupEmbed
-from .embeds import GenderRoleWasSetEmbed, SupportRoleWasSetEmbed, GuildSetupWasSuccessful, GuildWasAlreadySetup
+from .embeds import GenderRoleWasSetEmbed, SupportRoleWasSetEmbed, GuildSetupWasSuccessfulEmbed, GuildWasAlreadySetup
 from ...services.guilds_settings import initialize_guild_settings, get_guild_settings
 from ...services.users import get_or_create_user_by_discord_id, remove_user_by_discord_id
 
 
 class GuildSettingsCog(commands.Cog):
-    @commands.slash_command()
+    @commands.slash_command(name="setup", description="Настраивает сервер.")
     async def setup(self, inter: AppCmdInter) -> None:
         if inter.author.guild_permissions.administrator:
             async with session_factory() as session:
                 if await initialize_guild_settings(session, guild_id=inter.guild_id):
-                    await inter.response.send_message(embed=GuildSetupWasSuccessful())
+                    await inter.response.send_message(embed=GuildSetupWasSuccessfulEmbed())
                 else:
                     await inter.response.send_message(embed=GuildWasAlreadySetup())
         else:
@@ -27,11 +28,11 @@ class GuildSettingsCog(commands.Cog):
     ) -> None:
         pass
 
-    @set.sub_command()
+    @set.sub_command(name="female_role", description="Устанавливает роль для женского пола.")
     async def female_role(
         self,
         inter: AppCmdInter,
-        role: Role,
+        role: Role = Param(description="Роль, которая будет установлена как роль для женского пола."),
     ) -> None:
         if inter.author.guild_permissions.administrator:
             async with session_factory() as session:
@@ -44,11 +45,11 @@ class GuildSettingsCog(commands.Cog):
         else:
             await inter.response.send_message(embed=NotEnoughPermissionsEmbed(), ephemeral=True)
 
-    @set.sub_command()
+    @set.sub_command(name="male_role", description="Устанавливает роль для мужского пола.")
     async def male_role(
         self,
         inter: AppCmdInter,
-        role: Role,
+        role: Role = Param(description="Роль, которая будет установлена как роль для мужского пола."),
     ) -> None:
         if inter.author.guild_permissions.administrator:
             async with session_factory() as session:
@@ -61,11 +62,11 @@ class GuildSettingsCog(commands.Cog):
         else:
             await inter.response.send_message(embed=NotEnoughPermissionsEmbed(), ephemeral=True)
 
-    @set.sub_command()
+    @set.sub_command(name="support_role", description="Устанавливает роль для поддержки сервера.")
     async def support_role(
         self,
         inter: AppCmdInter,
-        role: Role,
+        role: Role = Param(description="Роль, которая будет установлена как роль для поддержки сервера."),
     ) -> None:
         if inter.author.guild_permissions.administrator:
             async with session_factory() as session:
@@ -81,6 +82,7 @@ class GuildSettingsCog(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member: Member) -> None:
         async with session_factory() as session:
+            # add member to database
             await get_or_create_user_by_discord_id(session, discord_id=member.id, guild_id=member.guild.id)
 
 
